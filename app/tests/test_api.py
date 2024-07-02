c54e453356135cc61996232423927bb02af30069
omognuni <melbong0069@gmail.com>
2024-07-02 15:57:34 +0900

    [Test] update

--- app/tests/test_api.py
+++ app/tests/test_api.py
@@ -1,24 +1,26 @@
 def test_create_short_url(client):
-    response = client.post("/shortens", json={"url": "https://example.com"})
+    response = client.post("/shorten", json={"url": "https://example.com"})
     assert response.status_code == 200
     assert "short_key" in response.json()
 
 
 def test_redirect_url(client):
-    post_response = client.post("/shortens", json={"url": "https://example.com"})
+    post_response = client.post("/shorten", json={"url": "https://example.com"})
     short_key = post_response.json()["short_key"]
     response = client.get(f"/{short_key}", follow_redirects=False)
     assert response.status_code == 301
 
 
 def test_get_urls(client):
-    response = client.get("/shortens")
+    response = client.get("/shorten")
     assert response.status_code == 200
     # assert isinstance(response.json(), list)
 
 
 def test_get_url_stats(client):
-    post_response = client.post("/shortens", json={"url": "https://example.com"})
+    post_response = client.post("/shorten", json={"url": "https://example.com"})
     short_key = post_response.json()["short_key"]
     response = client.get(f"/stats/{short_key}")
+    stats = response.json()["stats"]
     assert response.status_code == 200
+    assert stats == 0
--- app/tests/test_crud.py
+++ app/tests/test_crud.py
@@ -2,12 +2,14 @@ import pytest
 from datetime import datetime, timedelta
 from crud import create_short_url, fetch_original_url
 
+SHORT_URL_LENGTH = 8
+
 
 def test_create_short_url(test_db):
     url = "https://example.com"
     result = create_short_url(test_db, url)
     assert result.url == url
-    assert len(result.short_key) == 8
+    assert len(result.short_key) == SHORT_URL_LENGTH
 
 
 def test_fetch_original_url(test_db):
@@ -20,7 +22,7 @@ def test_fetch_original_url(test_db):
 def test_expired_url(test_db):
     url = "https://example-expired.com"
     expiry_date = datetime.now() - timedelta(days=1)
-    short_url = create_short_url(test_db, url, expiry=expiry_date)
+    short_url = create_short_url(test_db, url, expiry_date=expiry_date)
     result = fetch_original_url(test_db, short_url.short_key)
     assert result is None
 
